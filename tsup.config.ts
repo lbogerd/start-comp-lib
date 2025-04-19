1af91011fac6c51b129bf86604847442fb64fca0
Lucas Bogerd <l.bogerd@protonmail.com>
2025-04-19 16:54:25 +0200

    Add tsup for library building and update .gitignore
    - Introduced `tsup` version 8.4.0 for building libraries with new scripts `build:lib` and `dev:lib` in `package.json`.
    - Updated `.gitignore` to exclude the `dist/` directory.
    - Created a new `tsup.config.ts` file for configuration of the build process, specifying entry points and output settings.

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  // dynamically get all the files in the src/components/libs/tilburg directory
+  entry: ['src/components/libs/tilburg/**/*.tsx'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  splitting: true,
+  sourcemap: true,
+  clean: true,
+  external: ['react', 'react-dom'], // Add other external dependencies if needed
+  outDir: 'dist/tilburg', // Specify the output directory
+}); 
