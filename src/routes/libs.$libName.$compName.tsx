65b66d28e37969a25c366d4d338b4399da1f0d06
Lucas Bogerd <l.bogerd@protonmail.com>
2025-04-28 17:16:48 +0200

    rework: remove react-hook-form

--- src/routes/libs.$libName.$compName.tsx
+++ src/routes/libs.$libName.$compName.tsx
@@ -4,9 +4,7 @@ import { useEffect, useState } from 'react'
 import { getComp } from '~/logic/server/comps'
 
 import { ComponentDoc } from 'react-docgen-typescript'
-import { Form, useForm } from 'react-hook-form'
 import { CodeDisplay } from '~/components/libs/internal/code-display'
-import { PropField } from '~/components/libs/internal/prop-field'
 
 export const Route = createFileRoute('/libs/$libName/$compName')({
 	loader: async ({ params: { libName, compName } }) => {
@@ -21,32 +19,9 @@ function RouteComponent() {
 	const params = Route.useParams()
 	const data = Route.useLoaderData()
 
-	const form = useForm({
-		defaultValues: {
-			children: 'Lorem ipsum dolor sit amet',
-		},
-	})
-
 	const [Cmp, setCmp] = useState<ComponentType<any>[] | null>(null)
-	const [componentPropValues, setComponentPropValues] = useState<any>({})
 	const [isLoading, setIsLoading] = useState(true)
 
-	// Use watch to subscribe to all form changes
-	const watchedValues = form.watch()
-
-	// Fix: Only update componentPropValues when watchedValues actually changes
-	// This prevents infinite re-renders
-	useEffect(() => {
-		// Using a functional update to ensure we're not causing unnecessary updates
-		setComponentPropValues((prev: any) => {
-			// Only update if values are different
-			if (JSON.stringify(prev) === JSON.stringify(watchedValues)) {
-				return prev;
-			}
-
-			return watchedValues;
-		});
-	}, [watchedValues])
 
 	useEffect(() => {
 		setIsLoading(true)
@@ -69,37 +44,8 @@ function RouteComponent() {
 		<div>
 			<CodeDisplay>{data.sourceCode}</CodeDisplay>
 
-			<div>
-				<Form {...form}>
-
-						{JSON.parse(data.docs).map((doc: ComponentDoc) => (
-							<>
-								<pre>{JSON.stringify(doc, null, 2)}</pre>
-								<PropField
-									key="children"
-									meta={{
-										name: 'children',
-										type: { name: 'string' },
-										required: false,
-										description: '',
-										defaultValue: undefined,
-									}}
-									control={form.control}
-								/>
-								{Object.keys(doc.props).map((prop: string) => (
-									<PropField
-										key={prop}
-										meta={doc.props[prop]}
-										control={form.control}
-									/>
-								))}
-							</>
-						))}
-
-				</Form>
-			</div>
 			{Cmp && !isLoading ? (
-				Cmp.map((Cmp, index) => <Cmp key={index} {...componentPropValues} />)
+				Cmp.map((Cmp, index) => <Cmp key={index} />)
 			) : (
 				<span>Loading...</span>
 			)}
