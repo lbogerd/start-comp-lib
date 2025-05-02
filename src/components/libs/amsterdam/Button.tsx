e1b963e2a2c71eecdb595825a2244ea1970f6429
lbogerd <l.bogerd@protonmail.com>
2025-05-02 12:01:11 +0200

    fix: simplify button class management using cn utility

--- src/components/libs/amsterdam/Button.tsx
+++ src/components/libs/amsterdam/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { cn } from '~/logic/client/cn'
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
@@ -10,7 +11,10 @@ export const Button: React.FC<ButtonProps> = ({
 }) => {
 	return (
 		<button
-			className={`focus:ring-opacity-50 rounded bg-blue-600 px-4 py-2 font-semibold text-white transition duration-150 ease-in-out hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none disabled:cursor-not-allowed disabled:bg-blue-400 disabled:opacity-50 ${className} `}
+			className={cn(
+				'rounded bg-blue-600 px-4 py-2 font-semibold text-white transition duration-150 ease-in-out hover:bg-blue-700 focus:ring-2 focus:ring-blue-500/50 focus:outline-none disabled:cursor-not-allowed disabled:bg-blue-400 disabled:opacity-50',
+				className,
+			)}
 			disabled={disabled}
 			{...props}
 		>
