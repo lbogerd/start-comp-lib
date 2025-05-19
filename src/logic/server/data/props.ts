1de296ff473184583606c4cd73e8c5b783943735
lbogerd <l.bogerd@protonmail.com>
2025-05-19 12:32:57 +0200

    feat: Add code to get prop types

---
+++ src/logic/server/data/props.ts
@@ -0,0 +1,73 @@
+import {
+	ArrowFunction,
+	FunctionDeclaration,
+	FunctionExpression,
+	SourceFile,
+	VariableDeclaration,
+} from 'ts-morph'
+import ts from 'typescript'
+
+export function getFunctionDeclaration(
+	source: SourceFile,
+	functionName: string,
+): FunctionDeclaration | ArrowFunction | FunctionExpression | undefined {
+	const allFunctions = source.getFunctions()
+	const allVariables = source.getVariableDeclarations()
+
+	let fnDecl:
+		| FunctionDeclaration
+		| ArrowFunction
+		| FunctionExpression
+		| undefined = allFunctions.find((f) => f.getName() === functionName)
+
+	if (!fnDecl) {
+		const constFn = allVariables.find((v: VariableDeclaration) => {
+			const initializer = v.getInitializer()
+			return (
+				initializer &&
+				initializer.getType().getCallSignatures().length > 0 &&
+				v.getName() === functionName
+			)
+		})
+		if (constFn) {
+			const initializer = constFn.getInitializer()
+			if (initializer) {
+				if (initializer.isKind(ts.SyntaxKind.ArrowFunction)) {
+					fnDecl = initializer as ArrowFunction
+				} else if (initializer.isKind(ts.SyntaxKind.FunctionExpression)) {
+					fnDecl = initializer as FunctionExpression
+				}
+			}
+		}
+	}
+	return fnDecl
+}
+
+export function extractParamTypes(
+	fnDecl: FunctionDeclaration | ArrowFunction | FunctionExpression,
+) {
+	const params = fnDecl.getParameters()
+	return params.map((p) => {
+		const type = p.getType()
+		if (type.isObject()) {
+			const properties = type.getProperties()
+			const propertiesText = properties
+				.map(
+					(prop) =>
+						`${prop.getName()}: ${prop
+							.getDeclarations()[0]
+							.getType()
+							.getText()}`,
+				)
+				.join(', ')
+			return {
+				name: p.getName(),
+				type: `{ ${propertiesText} }`,
+			}
+		}
+		return {
+			name: p.getName(),
+			type: type.getText(),
+		}
+	})
+}
