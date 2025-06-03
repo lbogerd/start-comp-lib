a887c4a64aa12bc34e5394316d05b591276156ee
Lucas Bogerd <l.bogerd@protonmail.com>
2025-06-03 23:11:35 +0200

    feat: Add vite-plugin-static-copy for copying library files to server on prod

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,29 @@
 import { defineConfig } from '@tanstack/react-start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 export default defineConfig({
 	tsr: {
 		appDirectory: 'src',
 	},
 	vite: {
 		plugins: [
+			// @ts-expect-error - minor typing errors
 			tsConfigPaths({
 				projects: ['./tsconfig.json'],
 			}),
 			// @ts-ignore - minor typing errors
 			tailwindcss(),
+			// @ts-expect-error - minor typing errors
+			viteStaticCopy({
+				targets: [
+					{
+						src: 'src/libs',
+						dest: 'server', // This will copy to `[outDir]/server/libs`
+					},
+				],
+			}),
 		],
 	},
 })
